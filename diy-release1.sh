9b828ded87b518241a97ec3b7af428c3543102a4
cnkids <rclandy7@gmail.com>
2024-12-20 23:03:40 +0800

    Update diy-release1.sh

--- diy-release1.sh
+++ diy-release1.sh
@@ -13,5 +13,5 @@
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 #添加主题
-echo 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+#echo 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
