a30ea2dc887a3f0c24e65851516674039587631a
cnkids <rclandy7@gmail.com>
2026-06-21 11:00:00 +0800

    Update diy-release1.sh

--- diy-release1.sh
+++ diy-release1.sh
@@ -3,8 +3,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # 说明：
-# 此脚本在 feeds 更新前执行（Update feeds 之前）
-# 用于添加自定义软件源、下载额外源码等
+# 此脚本在 feeds 更新前执行（Load custom feeds 步骤）
+# 用于添加自定义软件源
 #
 
 # ========== 1. 修改默认 IP（可选，默认注释） ==========
@@ -14,11 +14,7 @@
 sed -i '/helloworld/d' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
-# ========== 3. 添加 Argon 主题（如果 feeds 里没有） ==========
-# 如果源码已经自带 argon，则此行可注释掉；若没有则克隆到 package 目录
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# ========== 4. （可选）添加其他第三方源 ==========
-# echo 'src-git other https://github.com/xxx/xxx.git' >> feeds.conf.default
+# ========== 3. 添加 Argon 主题（可选，如果在 workflow 中已经 git clone 了则不需要） ==========
+# 你的 workflow 中已经有 "install theme argon" 步骤克隆了 argon，所以这里不需要再重复
 
 exit 0
